bda858523878a7afd83865b10852a61d55c97e97
Davi C. Pes. <davicalo2d@gmail.com>
2024-07-08 19:37:35 -0300

    style color4

--- app/src/main/java/crud/example/com/ui/components/ExtendedFloatingActionButton.kt
+++ app/src/main/java/crud/example/com/ui/components/ExtendedFloatingActionButton.kt
@@ -3,6 +3,7 @@ package crud.example.com.ui.components
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -13,13 +14,19 @@ import androidx.compose.runtime.remember
 
 @Composable
 fun ExtendedActionButton(onClick: () -> Unit){
-    val listState = rememberLazyListState()
-    val expandedFab by remember { derivedStateOf { listState.firstVisibleItemIndex == 0 } }
-    androidx.compose.material3.ExtendedFloatingActionButton(
-        containerColor = MaterialTheme.colorScheme.secondary,
+//    val listState = rememberLazyListState()
+//    val expandedFab by remember { derivedStateOf { listState.firstVisibleItemIndex == 0 } }
+//    androidx.compose.material3.ExtendedFloatingActionButton(
+//        containerColor = MaterialTheme.colorScheme.secondary,
+//        onClick = { onClick() },
+//        expanded = expandedFab,
+//        icon = { Icon(Icons.Filled.Add, "Localized Description") },
+//        text = { Text("Adicionar", style = MaterialTheme.typography.bodyMedium) },
+//    )
+    FloatingActionButton(
         onClick = { onClick() },
-        expanded = expandedFab,
-        icon = { Icon(Icons.Filled.Add, "Localized Description") },
-        text = { Text("Adicionar", style = MaterialTheme.typography.bodyMedium) },
-    )
+        containerColor = MaterialTheme.colorScheme.secondary
+    ) {
+        Icon(Icons.Filled.Add, "Floating action button.")
+    }
 }
